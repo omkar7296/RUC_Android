b8bc44815137fc4f3b0f5bc604f816eaf882d782
omkar7296 <omkar.sawant6@gmail.com>
2018-03-03 20:25:16 -0500

    Initial commit

---
+++ app/src/main/java/com/upitt/ruc/Splash_Screen.java
@@ -0,0 +1,27 @@
+package com.upitt.ruc;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash__screen);
+
+        //using a handler to display splash-screen for 2 seconds.
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(Splash_Screen.this, Landing_Activity.class);
+                Splash_Screen.this.startActivity(mainIntent);
+                Splash_Screen.this.finish();
+            }
+        }, 2000);
+    }
+}
+
